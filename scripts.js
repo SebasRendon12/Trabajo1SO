e307dfbcedff31d58f0fa705df0412f5f75d23ae
SebasRendon12 <srendon@lukount.com>
2021-04-18 02:22:47 -0500

    Design Beta Version

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function mostrar(i) {
+  document.getElementById('Acciones' + i).style.display = 'block';
+}
+
+function ocultar(i) {
+  document.getElementById('Acciones' + i).style.display = 'none';
+}
