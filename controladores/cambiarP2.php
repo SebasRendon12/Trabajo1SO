4266b6c1ea62232b36397f404ab9a91d8ce4b987
Londunic <69688193+Londunic@users.noreply.github.com>
2021-08-01 22:00:03 -0500

    cambio

--- controladores/cambiarP2.php
+++ controladores/cambiarP2.php
@@ -18,29 +18,7 @@
             <div class="col-6">
             <br><br><br><br>
 
-            <?php
-
-/*                $raiz = $_POST["raiz"];
-                $nombre = $_POST["nombre"];
-                $formato = "";
-                $nuevoNombre = $_POST["nombre2"];
-                if (!is_dir($raiz . $nombre)) {
-                    $formato = explode(".", $nombre)[count(explode(".", $nombre)) - 1];
-                    $formato = "." . $formato;
-                }
-                $nuevoNombre .=  $formato;
-                if (!is_dir($raiz . $nuevoNombre . "." . $formato) && !is_file($raiz . $nuevoNombre)) {
-                    if (rename($raiz . $nombre, $raiz . $nuevoNombre)) {
-                        header("Location: ../explorador.php?ruta=$raiz");
-                    } else {
-                        echo "<h1>Error</h1>";
-                        echo "<a href='../explorador.php?ruta=$raiz'><h1>Volver</h1></a>";
-                    }
-                } else {
-                    echo "<h1>Ya existe un archivo con este nombre -> ( $nuevoNombre)</h1>";
-                    echo "<a href='../explorador.php?ruta=$raiz'><h1>Volver</h1></a>";
-                }
-*/                
+            <?php               
                 $raiz = $_POST["raiz"];
                 $nombre = $_POST["nombre"];
                 $nuevoUsuario = $_POST["propetario"];
